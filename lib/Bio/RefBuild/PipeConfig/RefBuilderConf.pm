521b01cf3663ca3388d5b272c224f17c85075805
David Richardson <davidr@ebi.ac.uk>
2015-07-16 15:16:51 +0100

    Reorganise pipeline into multiple large steps

--- lib/Bio/RefBuild/PipeConfig/RefBuilderConf.pm
+++ lib/Bio/RefBuild/PipeConfig/RefBuilderConf.pm
@@ -5,24 +5,70 @@ use warnings;
 
 use base ('Bio::EnsEMBL::Hive::PipeConfig::HiveGeneric_conf');
 
+my %foo = {
+    "dir_annot_index_rsem" =>
+"/nfs/production/reseq-info/work/davidr/ref_builder/ref_files/Homo_sapiens/ta1/test1_gencode/rsem",
+    "dir_annot_index_rsem_polya" =>
+"/nfs/production/reseq-info/work/davidr/ref_builder/ref_files/Homo_sapiens/ta1/test1_gencode/rsem_polya",
+    "dir_annot_index_star" =>
+"/nfs/production/reseq-info/work/davidr/ref_builder/ref_files/Homo_sapiens/ta1/test1_gencode/star",
+    "dir_annotation" =>
+"/nfs/production/reseq-info/work/davidr/ref_builder/ref_files/Homo_sapiens/ta1/annotation",
+    "dir_annotation_base" =>
+"/nfs/production/reseq-info/work/davidr/ref_builder/ref_files/Homo_sapiens/ta1/test1_gencode",
+    "dir_base" =>
+"/nfs/production/reseq-info/work/davidr/ref_builder/ref_files/Homo_sapiens/ta1",
+    "dir_genome_fasta" =>
+"/nfs/production/reseq-info/work/davidr/ref_builder/ref_files/Homo_sapiens/ta1/genome_fasta",
+    "dir_index_bismark" =>
+"/nfs/production/reseq-info/work/davidr/ref_builder/ref_files/Homo_sapiens/ta1/genome_index/bismark",
+    "dir_index_bowtie1" =>
+"/nfs/production/reseq-info/work/davidr/ref_builder/ref_files/Homo_sapiens/ta1/genome_index/bowtie1",
+    "dir_index_bowtie2" =>
+"/nfs/production/reseq-info/work/davidr/ref_builder/ref_files/Homo_sapiens/ta1/genome_index/bowtie2",
+    "dir_index_bwa" =>
+"/nfs/production/reseq-info/work/davidr/ref_builder/ref_files/Homo_sapiens/ta1/genome_index/bwa",
+    "dir_mappability" =>
+"/nfs/production/reseq-info/work/davidr/ref_builder/ref_files/Homo_sapiens/ta1/mappability",
+    "dir_split_genome_fasta" =>
+"/nfs/production/reseq-info/work/davidr/ref_builder/ref_files/Homo_sapiens/ta1/genome_fasta/split_fasta",
+    "fai" =>
+"/nfs/production/reseq-info/work/davidr/ref_builder/ref_files/Homo_sapiens/ta1/genome_fasta/ta1.fa.gz.fai",
+    "fasta" =>
+"/nfs/production/reseq-info/work/davidr/ref_builder/ref_files/Homo_sapiens/ta1/genome_fasta/ta1.fa",
+    "gtf" =>
+"/nfs/production/reseq-info/work/davidr/ref_builder/ref_files/Homo_sapiens/ta1/test1_gencode/ta1_test1_gencode.gtf",
+    "gtf_gz" =>
+"/nfs/production/reseq-info/work/davidr/ref_builder/ref_files/Homo_sapiens/ta1/test1_gencode/ta1_test1_gencode.gtf.gz",
+    "manifest" =>
+"/nfs/production/reseq-info/work/davidr/ref_builder/ref_files/Homo_sapiens/ta1/files.manifest",
+    "ref_flat" =>
+"/nfs/production/reseq-info/work/davidr/ref_builder/ref_files/Homo_sapiens/ta1/test1_gencode/ta1_test1_gencode.ref_flat.gz",
+    "rrna_interval" =>
+"/nfs/production/reseq-info/work/davidr/ref_builder/ref_files/Homo_sapiens/ta1/test1_gencode/ta1_test1_gencode.rrna.interval"
+};
+
 sub pipeline_wide_parameters {
     my ($self) = @_;
     return {
         %{ $self->SUPER::pipeline_wide_parameters },
 
         #required executables
-        samtools      => $self->o('samtools'),
-        bowtie1_dir   => $self->o('bowtie1_dir'),
-        bowtie2_dir   => $self->o('bowtie2_dir'),
-        rsem_dir      => $self->o('rsem_dir'),
-        picard        => $self->o('picard'),
-        java          => $self->o('java'),
-        bedtools      => $self->o('bedtools'),
-        bwa           => $self->o('bwa'),
-        bgzip         => $self->o('bgzip'),
-        star          => $self->o('star'),
-        gtfToGenePred => $self->o('gtfToGenePred'),
-        bismark_dir   => $self->o('bismark_dir'),
+        samtools                  => $self->o('samtools'),
+        bowtie1_dir               => $self->o('bowtie1_dir'),
+        bowtie2_dir               => $self->o('bowtie2_dir'),
+        rsem_dir                  => $self->o('rsem_dir'),
+        picard                    => $self->o('picard'),
+        java                      => $self->o('java'),
+        bedtools                  => $self->o('bedtools'),
+        bwa                       => $self->o('bwa'),
+        bgzip                     => $self->o('bgzip'),
+        star                      => $self->o('star'),
+        gtfToGenePred             => $self->o('gtfToGenePred'),
+        bismark_dir               => $self->o('bismark_dir'),
+        output_root               => $self->o('output_root'),
+        assembly_index_programs   => $self->o('assembly_index_programs'),
+        annotation_index_programs => $self->o('annotation_index_programs'),
     };
 }
 
@@ -30,64 +76,131 @@ sub default_options {
     my ($self) = @_;
     return {
         %{ $self->SUPER::default_options() },
-        index_programs =>
-          [qw(bismark bwa bowtie1 bowtie2 rsem rsem_polya star)],
-        pipeline_name => 'ref_builder',
+        assembly_index_programs   => [qw(bismark bwa bowtie1 bowtie2)],
+        annotation_index_programs => [qw(rsem rsem_polya star)],
+        pipeline_name             => 'ref_builder',
     };
 }
+
+=head Parameters
+  Required options:
+  executables / dirs ( always required, should be set in options at setup )
+    samtools
+    star
+    bedtools
+    bwa
+    java
+    bgzip
+    gtfToGenePred
+    bismark_dir
+    bowtie1_dir
+    bowtie2_dir
+    rsem_dir
+    picard(jar)
+    output_root
+
+  Required in input:
+  start_* parameters( we always need these )
+    root_output_dir
+    species_name
+    assembly_name
+
+  start_assembly parameters
+    fasta_file
+    fasta_uri
+
+  start_annotation parameters
+    gtf_file
+    annotation_name
+
+  Parameters created by start_assembly:
+    dir_base
+    dir_annotation
+    dir_genome_fasta
+    dir_mappability
+    dir_split_genome_fasta
+    dir_index_bismark
+    dir_index_bowtie1
+    dir_index_bowtie2
+    dir_index_bwa
+
+    manifest
+    assembly_base_name
+    bgzip_fasta
+    fasta
+    chrom_sizes
+    dict
+    fai
+
+  Additional Parameters created by start_annotation:
+    dir_index_rsem
+    dir_index_rsem_polya
+    dir_index_star
+
+    annotation_base_name
+    gtf                 
+    gtf_gz
+    ref_flat
+    rrna_interval
+
+=cut
 
 sub pipeline_analyses {
     my ($self) = @_;
     return [
+        #entry points
         {
-            -logic_name => 'start',
+            -logic_name => 'start_assembly',
             -module     => 'Bio::RefBuild::Process::RefBuilderPreFlightChecks',
             -rc_name    => 'default',
-            -flow_into  => ['make_dirs'],
+            -parameters => {
+                do_assembly   => 1,
+                do_annotation => 0,
+            },
+            -flow_into => ['process_fasta'],
         },
         {
-            -logic_name => 'make_dirs',
-            -module     => 'Bio::RefBuild::Process::CreateDirs',
+            -logic_name => 'start_annotation',
+            -module     => 'Bio::RefBuild::Process::RefBuilderPreFlightChecks',
             -rc_name    => 'default',
-            -parameters => { index_programs => $self->o('index_programs') },
-            -flow_into  => [ 'cp_annotation', ],
-        },
-        {
-            -logic_name => 'cp_annotation',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -rc_name    => 'default',
-            -parameters => { cmd => 'cp #gtf_file# #gtf#', },
-            -flow_into  => [ 'unzip_annotation', 'ref_flat' ],
-        },
-        {
-            -logic_name => 'unzip_annotation',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -rc_name    => 'default',
-            -parameters => { cmd => 'gunzip -c #gtf# > #temp_gtf#', },
-            -flow_into  => [ 'process_fasta', ],
-        },
-        {
-            -logic_name => 'ref_flat',
+            -parameters => {
+                do_assembly   => 0,
+                do_annotation => 1,
 
-# adapted fromhttps://gist.github.com/igordot/4467f1b02234ff864e61 ref flat from gtf
+            },
+            -flow_into => [ 'cp_annotation', 'unzip_annotation' ],
+        },
+        {
+            -logic_name => 'start_manifest',
+            -module     => 'Bio::RefBuild::Process::RefBuilderPreFlightChecks',
+            -rc_name    => 'default',
+            -parameters => {
+                do_assembly   => 1,
+                do_annotation => 0,
+            },
+            -flow_into => ['write_manifest'],
+        },
+
+        #final job
+        {
+            -logic_name => 'write_manifest',
             -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -rc_name    => '400Mb_job',
+            -rc_name    => 'default',
             -parameters => {
                 cmd =>
-'#gtfToGenePred# -genePredExt -geneNameAsName2 #gtf# /dev/stdout | awk \'BEGIN{OFS="\t";FS="\t"}{print $12,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10}\' | gzip -c > #dir_annotation#/#annotation_name#.ref_flat.gz',
+'find #dir_base# -type f -printf \'%p\t%s\t\' -execdir sh -c \'md5sum "{}" | sed s/\ .*//\' \; > #manifest#',
             },
         },
+
+        #assembly processes
         {
             -logic_name => 'process_fasta',
             -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
             -parameters => {
                 cmd =>
-'gunzip -c #fasta_file# | tee #temp_fasta# | #bgzip# -c > #bgzip_fasta#',
+'gunzip -c #fasta_file# | tee #fasta# | #bgzip# -c > #bgzip_fasta#',
             },
-            -flow_into => {
-                '1->A' => [ 'support_files', 'indexing', ],
-                'A->1' => ['rm_temp_files']
-            },
+            -flow_into => [ 'support_files', 'assembly_indexing', ],
         },
         {
             -logic_name  => 'support_files',
@@ -96,14 +209,12 @@ sub pipeline_analyses {
             -flow_into   => [ 'samtools_fai', 'picard_dict' ],
         },
         {
-            -logic_name  => 'indexing',
+            -logic_name  => 'assembly_indexing',
             -module      => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
             -meadow_type => 'LOCAL',
             -flow_into   => [
                 'bowtie1_index', 'bowtie2_index',
-                'star_index',    'bwa_index',
-                'rsem_index',    'rsem_polya_index',
-                'pre_bismark_index'
+                'bwa_index',     'pre_bismark_index',
             ],
         },
         {
@@ -127,16 +238,6 @@ sub pipeline_analyses {
                 cmd =>
 'rm -f #dict# ; #java# -jar #picard# CreateSequenceDictionary REFERENCE=#bgzip_fasta# OUTPUT=#dict# GENOME_ASSEMBLY="#assembly_name#" SPECIES="#species_name#" URI="#fasta_uri#"',
             },
-            -flow_into => ['rrna_interval'],
-        },
-        {
-            -logic_name => 'rrna_interval',
-            -module     => 'Bio::RefBuild::Process::GtfToRrnaIntervalProcess',
-            -rc_name    => '200Mb_job',
-            -parameters => {
-                rrna_interval =>
-                  '#dir_annotation#/#assembly_name#.rrna.interval'
-            },
         },
         {
             -logic_name => 'bwa_index',
@@ -144,7 +245,7 @@ sub pipeline_analyses {
             -rc_name    => '5Gb_job',
             -parameters => {
                 cmd =>
-'#bwa# index -p #dir_index_bwa#/#assembly_name# #bgzip_fasta#',
+'#bwa# index -p #dir_index_bwa#/#assembly_base_name# #bgzip_fasta#',
             },
         },
         {
@@ -153,7 +254,7 @@ sub pipeline_analyses {
             -rc_name    => '6Gb_job',
             -parameters => {
                 cmd =>
-'#bowtie1_dir#/bowtie-build -q #temp_fasta# #dir_index_bowtie1#/#assembly_name#',
+'#bowtie1_dir#/bowtie-build -q #fasta# #dir_index_bowtie1#/#assembly_base_name#',
             },
         },
         {
@@ -162,34 +263,7 @@ sub pipeline_analyses {
             -rc_name    => '6Gb_job',
             -parameters => {
                 cmd =>
-'#bowtie2_dir#/bowtie2-build -q #temp_fasta# #dir_index_bowtie2#/#assembly_name#',
-            },
-        },
-        {
-            -logic_name => 'star_index',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -rc_name    => 'star_job',
-            -parameters => {
-                cmd =>
-'#star# --runMode genomeGenerate --runThreadN 4 --genomeDir #dir_index_star# --genomeFastaFiles #temp_fasta# --sjdbGTFfile #temp_gtf#',
-            },
-        },
-        {
-            -logic_name => 'rsem_index',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -rc_name    => '1Gb_job',
-            -parameters => {
-                cmd =>
-'#rsem_dir#/rsem-prepare-reference -q --gtf #temp_gtf# --bowtie --bowtie-path #bowtie1_dir# --bowtie2 --bowtie2-path #bowtie2_dir# #temp_fasta# #dir_index_rsem#/#annotation_name#'
-            },
-        },
-        {
-            -logic_name => 'rsem_polya_index',
-            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
-            -rc_name    => '1Gb_job',
-            -parameters => {
-                cmd =>
-'#rsem_dir#/rsem-prepare-reference -q --polyA --gtf #temp_gtf# --bowtie --bowtie-path #bowtie1_dir# --bowtie2 --bowtie2-path #bowtie2_dir# #temp_fasta# #dir_index_rsem_polya#/#annotation_name#'
+'#bowtie2_dir#/bowtie2-build -q #fasta# #dir_index_bowtie2#/#assembly_base_name#',
             },
         },
         {
@@ -197,7 +271,7 @@ sub pipeline_analyses {
             -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
             -rc_name    => 'default',
             -parameters => {
-                cmd => 'cp #temp_fasta# #dir_index_bismark#/#assembly_name#.fa',
+                cmd => 'cp #fasta# #dir_index_bismark#/#assembly_base_name#.fa',
             },
             -flow_into => ['bismark_index'],
         },
@@ -209,12 +283,86 @@ sub pipeline_analyses {
                 cmd =>
 '#bismark_dir#/bismark_genome_preparation --path_to_bowtie #bowtie1_dir# --yes_to_all #dir_index_bismark#',
             },
+            -flow_into => ['post_bismark_index'],
         },
         {
-            -logic_name => 'rm_temp_files',
+            -logic_name => 'post_bismark_index',
             -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
             -rc_name    => 'default',
-            -parameters => { cmd => 'rm #temp_fasta# #temp_gtf#', }
+            -parameters =>
+              { cmd => 'rm -f #dir_index_bismark#/#assembly_base_name#.fa', },
+        },
+
+        #annotation processes
+        {
+            -logic_name => 'cp_annotation',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+            -rc_name    => 'default',
+            -parameters => { cmd => 'cp #gtf_file# #gtf_gz#', },
+            -flow_into  => ['ref_flat'],
+        },
+        {
+            -logic_name => 'unzip_annotation',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+            -rc_name    => 'default',
+            -parameters => { cmd => 'gunzip -c #gtf_file# > #gtf#', },
+            -flow_into  => [ 'annotation_indexing', 'rrna_interval' ],
+        },
+        {
+            -logic_name => 'ref_flat',
+
+# adapted fromhttps://gist.github.com/igordot/4467f1b02234ff864e61 ref flat from gtf
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+            -rc_name    => '400Mb_job',
+            -parameters => {
+                cmd =>
+'#gtfToGenePred# -genePredExt -geneNameAsName2 #gtf# /dev/stdout | awk \'BEGIN{OFS="\t";FS="\t"}{print $12,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10}\' | gzip -c > #ref_flat#',
+            },
+        },
+        {
+            -logic_name  => 'annotation_indexing',
+            -module      => 'Bio::EnsEMBL::Hive::RunnableDB::Dummy',
+            -meadow_type => 'LOCAL',
+            -flow_into   => [ 'rsem_index', 'rsem_polya_index', 'star_index', ],
+        },
+
+        {
+            -logic_name => 'rrna_interval',
+            -module     => 'Bio::RefBuild::Process::GtfToRrnaIntervalProcess',
+            -rc_name    => '200Mb_job',
+            -parameters => {
+                gtf           => '#gtf',
+                dict          => '#dict#',
+                rrna_interval => '#rrna_interval#'
+            },
+        },
+
+        {
+            -logic_name => 'star_index',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+            -rc_name    => 'star_job',
+            -parameters => {
+                cmd =>
+'#star# --runMode genomeGenerate --runThreadN 4 --genomeDir #dir_annot_index_star# --genomeFastaFiles #fasta# --sjdbGTFfile #gtf#',
+            },
+        },
+        {
+            -logic_name => 'rsem_index',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+            -rc_name    => '1Gb_job',
+            -parameters => {
+                cmd =>
+'#rsem_dir#/rsem-prepare-reference -q --gtf #gtf# --bowtie --bowtie-path #bowtie1_dir# --bowtie2 --bowtie2-path #bowtie2_dir# #fasta# #dir_annot_index_rsem#/#annotation_base_name#'
+            },
+        },
+        {
+            -logic_name => 'rsem_polya_index',
+            -module     => 'Bio::EnsEMBL::Hive::RunnableDB::SystemCmd',
+            -rc_name    => '1Gb_job',
+            -parameters => {
+                cmd =>
+'#rsem_dir#/rsem-prepare-reference -q --polyA --gtf #gtf# --bowtie --bowtie-path #bowtie1_dir# --bowtie2 --bowtie2-path #bowtie2_dir# #fasta# #dir_annot_index_rsem_polya#/#annotation_base_name#_polya'
+            },
         },
 
     ];
--- lib/Bio/RefBuild/Process/RefBuilderPreFlightChecks.pm
+++ lib/Bio/RefBuild/Process/RefBuilderPreFlightChecks.pm
@@ -2,10 +2,10 @@ package Bio::RefBuild::Process::RefBuilderPreFlightChecks;
 
 use strict;
 use warnings;
-
+use File::Path qw(make_path);
 use base ('Bio::EnsEMBL::Hive::Process');
 
-sub fetch_input {
+sub check_exes {
     my ($self) = @_;
 
     # do the executables exist, and are they actually executable?
@@ -20,14 +20,14 @@ sub fetch_input {
 
     );
     for my $executable ( values %executables ) {
-        $self->check_executable($executable);
+        if ( !-x $executable ) {
+            $self->throw("$executable is not executable");
+        }
     }
+}
 
-    $self->param_required('picard');
-    $self->param_required('assembly_name');
-    $self->param_required('annotation_name');
-    $self->param_required('species_name');
-    $self->param_required('fasta_uri');
+sub check_dirs {
+    my ($self) = @_;
 
     my %dirs = (
         bismark_dir     => $self->param_required('bismark_dir'),
@@ -41,6 +41,35 @@ sub fetch_input {
             $self->throw("$dir is not a directory");
         }
     }
+}
+
+sub fetch_core_requirements {
+    my ($self) = @_;
+
+    $self->check_exes();
+    $self->check_dirs();
+    $self->param_required('picard');
+
+    my $species       = $self->param_required('species_name');
+    my $assembly_name = $self->param_required('assembly_name');
+
+    my $as = $self->sanitize_file_name($assembly_name);
+    my $sp = $self->sanitize_file_name($species);
+
+    $self->param( "assembly_filename_base", $as );
+
+    my $root_output_dir = $self->param_required('output_root');
+
+    my $assembly_output_dir = "$root_output_dir/$sp/$as";
+    $self->param( "assembly_output_dir", $assembly_output_dir );
+}
+
+sub fetch_assembly_requirements {
+    my ($self) = @_;
+
+    $self->param_required('fasta_uri');
+
+    my $as_index_programs = $self->param_required('assembly_index_programs');
 
     my $fasta_file = $self->param_required('fasta_file');
     if ( $fasta_file !~ /\.fa\.gz$/ ) {
@@ -51,40 +80,134 @@ sub fetch_input {
     if ( !-e $fasta_file ) {
         $self->throw("$fasta_file does not exist");
     }
+}
+
+sub fetch_annotation_requirements {
+    my ($self) = @_;
+
+    my $annot_index_programs =
+      $self->param_required('annotation_index_programs');
+
+    my $assembly_output_dir = $self->param("assembly_output_dir");
+    my $annotation          = $self->param_required("annotation_name");
+    my $as                  = $self->param("assembly_filename_base");
+
+    my $an = $self->sanitize_file_name($annotation);
+    $self->param( "annotation_output_dir",    "$assembly_output_dir/annotation/$an" );
+    $self->param( "annotation_filename_base", $as . '_' . $an );
+
     my $gtf_file = $self->param_required('gtf_file');
     if ( $gtf_file !~ /\.gtf\.gz$/ ) {
         $self->throw(
-            "$fasta_file should be a gzipped GTF file with the suffix .fa.gz");
+            "$gtf_file should be a gzipped GTF file with the suffix .fa.gz");
+    }
+    if ( !-e $gtf_file ) {
+        $self->throw("$gtf_file does not exist");
+    }
+}
+
+sub fetch_input {
+    my ($self) = @_;
+
+    $self->fetch_core_requirements();
+
+    #core requirements
+
+    #assembly mode
+    if ( $self->param("do_assembly") ) {
+        $self->fetch_assembly_requirements();
+    }
+
+    #annotation mode
+    if ( $self->param("do_annotation") ) {
+        $self->fetch_annotation_requirements();
     }
 
 }
 
-sub check_executable {
-    my ( $self, $executable ) = @_;
+sub run {
+    my ($self) = @_;
 
-    if ( !-x $executable ) {
-        $self->throw("$executable is not executable");
+    my $as_index_programs   = $self->param('assembly_index_programs');
+    my $assembly_output_dir = $self->param("assembly_output_dir");
+
+    my %dirs_created;
+
+    $dirs_created{"dir_base"} = $assembly_output_dir;
+
+    for my $dir (qw(genome_fasta mappability annotation)) {
+        my $target = "$assembly_output_dir/$dir";
+        $dirs_created{"dir_$dir"} = $target;
     }
+    $dirs_created{"dir_split_genome_fasta"} =
+      $dirs_created{"dir_genome_fasta"} . '/split_fasta';
+
+    for my $index_prog (@$as_index_programs) {
+        my $target = "$assembly_output_dir/genome_index/$index_prog";
+        $dirs_created{"dir_index_$index_prog"} = $target;
+    }
+
+    if ( $self->param("do_annotation") ) {
+        my $annotation_output_dir = $self->param("annotation_output_dir");
+        my $an_index_programs     = $self->param("annotation_index_programs");
+
+        $dirs_created{"dir_annotation_base"} = $annotation_output_dir;
+
+        for my $index_prog (@$an_index_programs) {
+            my $target = "$annotation_output_dir/$index_prog";
+            $dirs_created{"dir_annot_index_$index_prog"} = $target;
+        }
+    }
+
+    make_path($_) for ( values %dirs_created );
+
+    $self->param( 'dirs_created', \%dirs_created );
+}
+
+sub sanitize_file_name {
+    my ( $self, $name ) = @_;
+    $name =~ s/\W/_/;    # convert non-word chars to _
+    $name =~ s/_+/_/;    # remove multiple _ in a row
+    $name =~ s/^_+//;    # remove initial _
+    $name =~ s/_+$//;    # remove trailing _
+
+    return $name;
 }
 
 sub write_output {
     my ($self) = @_;
 
-    my $root            = $self->param_required('output_root');
-    my $assembly        = $self->param_required('assembly_name');
-    my $annotation_name = $self->param_required('annotation_name');
+    my %dirs_created = %{ $self->param('dirs_created') };
 
-    my %expected_files = (
-        bgzip_fasta => "$root/$assembly/fasta/$assembly.fa.gz",
-        fai         => "$root/$assembly/fasta/$assembly.fa.gz.fai",
-        gtf         => "$root/$assembly/annotation/$annotation_name.gtf.gz",
-        dict        => "$root/$assembly/fasta/$assembly.dict",
-        chrom_sizes => "$root/$assembly/support/$assembly.sizes",
-        temp_fasta  => "$root/$assembly/temp_fasta.fa",
-        temp_gtf    => "$root/$assembly/temp_gtf.gtf",
+    my $fasta_root          = $dirs_created{dir_genome_fasta};
+    my $assembly            = $self->param_required('assembly_filename_base');
+    my $assembly_output_dir = $self->param("assembly_output_dir");
+
+    my %params_out = (
+        assembly_base_name => $assembly,
+        bgzip_fasta        => "$fasta_root/$assembly.fa.gz",
+        fai                => "$fasta_root/$assembly.fa.gz.fai",
+        dict               => "$fasta_root/$assembly.dict",
+        chrom_sizes        => "$fasta_root/$assembly.sizes",
+        fasta              => "$fasta_root/$assembly.fa",
+        manifest           => "$assembly_output_dir/files.manifest"
     );
 
-    $self->dataflow_output_id( \%expected_files, 1 );
+    if ( $self->param("do_annotation") ) {
+        my $annotation_root = $dirs_created{"dir_annotation_base"};
+
+        my $annotation = $self->param_required('annotation_filename_base');
+
+        $params_out{annotation_base_name} = $annotation;
+        $params_out{gtf}                  = "$annotation_root/$annotation.gtf";
+        $params_out{gtf_gz}   = "$annotation_root/$annotation.gtf.gz";
+        $params_out{ref_flat} = "$annotation_root/$annotation.ref_flat.gz";
+        $params_out{rrna_interval} =
+          "$annotation_root/$annotation.rrna.interval";
+
+    }
+
+    $self->dataflow_output_id( { %dirs_created, %params_out }, 1 );
 }
 
 1;
